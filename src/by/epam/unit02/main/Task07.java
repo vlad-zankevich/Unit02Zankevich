02c64d81f837c0cc8f2ccd68c31eb3553cded296
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 22:06:26 +0300

    Update Task07.java
    Task 7 commit

--- src/by/epam/unit02/main/Task07.java
+++ src/by/epam/unit02/main/Task07.java
@@ -2,6 +2,32 @@ package by.epam.unit02.main;
 
 public class Task07 {
     public static void main(String[] args) {
+        double x = -2;
+        double y = 2;
+        double z = -3;
+
+        if (x > 0) {
+            x = Math.pow(x, 2);
+        } else {
+            x = Math.pow(x, 4);
+        }
+
+        if (y > 0) {
+            y = Math.pow(y, 2);
+        } else {
+            y = Math.pow(y, 4);
+        }
+
+        if (z > 0) {
+            z = Math.pow(z, 2);
+        } else {
+            z = Math.pow(z, 4);
+        }
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
 
     }
 }
