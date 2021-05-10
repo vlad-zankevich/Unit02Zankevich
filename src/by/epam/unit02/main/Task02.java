84001b47ecab3568c4cbd19267b3ef77d8b42620
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 18:31:48 +0300

    Update Task02.java

--- src/by/epam/unit02/main/Task02.java
+++ src/by/epam/unit02/main/Task02.java
@@ -2,6 +2,30 @@ package by.epam.unit02.main;
 
 public class Task02 {
     public static void main(String[] args){
+        double x;
+        double y;
+        double max;
+        double min;
+
+        x = 3;
+        y = 6;
+
+        if (x > y) {
+            max = x;
+            min = y;
+            System.out.println("x = " + x);
+            System.out.println("y = " + y);
+        }else {
+            min = x;
+            max = y;
+
+            x = max;
+            y = min;
+
+            System.out.println("x = " + x);
+            System.out.println("y = " + y);
+
+        }
 
     }
 }
