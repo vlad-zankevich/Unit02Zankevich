92376905ca300c8fd5df474ec6828dccc394906d
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 19:55:52 +0300

    Commit Task 4

--- src/by/epam/unit02/main/Task04.java
+++ src/by/epam/unit02/main/Task04.java
@@ -2,6 +2,16 @@ package by.epam.unit02.main;
 
 public class Task04 {
     public static void main(String[] args){
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = -3;
+        int y2 = -4;
+
+        int x = x2 - x1;
+        int y = y2 - y1;
+        double l = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+        System.out.println("The distance between two points = " + l);
 
     }
 }
