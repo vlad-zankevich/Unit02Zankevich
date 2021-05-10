6b0c4aabd39b12105b69477dab3228d8883dba73
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 18:05:29 +0300

    Update Task01.java

--- src/by/epam/unit02/main/Task01.java
+++ src/by/epam/unit02/main/Task01.java
@@ -2,6 +2,19 @@ package by.epam.unit02.main;
 
 public class Task01 {
     public static void main(String[] args){
+        float a;
+        float b;
+        float c;
+        float z;
+
+        a = 11;
+        b = 2;
+        c = 5;
+
+        z = (a -3) * (b / 2) + c;
+
+
+        System.out.println("z = " + z);
 
     }
 }
