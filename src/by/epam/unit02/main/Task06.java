70f5bd9b05f001e6ef9e1c44a7775bdf60765427
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 22:22:22 +0300

    Update Task06.java
    added checking for 0 in denominator

--- src/by/epam/unit02/main/Task06.java
+++ src/by/epam/unit02/main/Task06.java
@@ -3,15 +3,22 @@ package by.epam.unit02.main;
 public class Task06 {
     public static void main(String[] args){
         int M = 13;
-        int N = 3;
-        double A = (double) M / (double) N;
-        int B = (int)A;
-        double C = A - B;
-        C = C * 10;
-        int D = (int)C;
+        int N = 0;
+        double A;
 
-        System.out.println("The highest digit of the fractional part = " + D);
-        System.out.println("The lowest digit of the integer part = " + B % 10);
+        if (N != 0) {
+            A = (double) M / (double) N;
+            int B = (int)A;
+            double C = A - B;
+            C = C * 10;
+            int D = (int)C;
+
+            System.out.println("The highest digit of the fractional part = " + D);
+            System.out.println("The lowest digit of the integer part = " + B % 10);
+        }else {
+            System.out.println("The denominator is zero. ");
+            A = Double.NaN;
+        }
 
     }
 }
