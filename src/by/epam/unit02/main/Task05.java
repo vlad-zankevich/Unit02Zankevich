d8d5505baea01fd57b22f3f3dad73524c83ecdac
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-10 21:23:39 +0300

    Task 5
    Commit Task 5

--- src/by/epam/unit02/main/Task05.java
+++ src/by/epam/unit02/main/Task05.java
@@ -2,6 +2,13 @@ package by.epam.unit02.main;
 
 public class Task05 {
     public static void main(String[] args){
+        double x;
+        double y;
+
+        x = 2;
+        y = 3;
+
+        System.out.println(((Math.sin(x) + Math.cos(y))/(Math.cos(x) - Math.sin(y))) * Math.tan(x * y));
 
     }
 }
