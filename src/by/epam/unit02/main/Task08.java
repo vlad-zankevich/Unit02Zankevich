8a8e3305b971418dd66d6e4e116b7145f1038d97
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-15 22:25:31 +0300

    Update Task08.java
    Task 8 commited

--- src/by/epam/unit02/main/Task08.java
+++ src/by/epam/unit02/main/Task08.java
@@ -1,7 +1,27 @@
 package by.epam.unit02.main;
 
 public class Task08 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        double x1 = -2;
+        double x2 = 2;
+        double y1 = 0;
+        double y2 = 4;
+        double x3 = -4;
+        double x4 = 4;
+        double y3 = 0;
+        double y4 = -3;
+
+        double pointX = 0;
+        double pointY = 0;
+
+        if (((pointX >= x1) && (pointX <= x2)) && ((pointY >= y1) && (pointY <= y2))) {
+            System.out.println("true");
+        } else if (((pointX >= x3) && (pointX <= x4)) && ((pointY >= y4) && (pointY <= y3))) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
+
