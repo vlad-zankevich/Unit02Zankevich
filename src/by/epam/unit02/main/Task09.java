5fa882e43b23387ae69227b6ed217185ccf1f459
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-15 23:09:52 +0300

    Update Task09.java
    Task 9 commited

--- src/by/epam/unit02/main/Task09.java
+++ src/by/epam/unit02/main/Task09.java
@@ -2,6 +2,20 @@ package by.epam.unit02.main;
 
 public class Task09 {
     public static void main(String[] args){
+        double x;
+        double y;
+
+        x = -4;
+
+        if (x <= -3) {
+            y = 9;
+            System.out.println("F(x)= " + y);
+        } else if (x > 3) {
+            y = 1 / (Math.pow(x, 2) + 1);
+            System.out.println("F(x)= " + y);
+        } else {
+            System.out.println("There is no F(x) value for x = " + x );
+        }
 
     }
 }
